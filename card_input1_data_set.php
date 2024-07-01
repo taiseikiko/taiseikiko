6a41d1d652ab90643fad4951018c8d4fd1c69001
taiseikiko <htetwai@taiseikiko.com>
2024-07-01 13:02:28 +0900

    change case in card_input1

--- card_input1_data_set.php
+++ card_input1_data_set.php
@@ -4,8 +4,8 @@ $pdo = new PDO(DNS, USER_NAME, PASSWORD, get_pdo_options());
 
 function map_procurement_status($status) {
   switch ($status) {
-    // case 1:
-    //   return '技術部承認待ち';
+    case 1:
+      return '技術部入力待ち';
     case 2:
       return '技術部承認済み';
     case 3:
@@ -15,14 +15,14 @@ function map_procurement_status($status) {
     case 5:
       return '中止';
     default:
-      return '技術部入力待ち';
+      return '';
   }
 }
 
 function map_card_status($status) {
   switch ($status) {
-    // case 1:
-    //   return '資材部承認待ち';
+    case 1:
+      return '資材部承認待ち';
     case 2:
       return '資材部承認済み';
     case 3:
@@ -32,7 +32,7 @@ function map_card_status($status) {
     case 5:
       return '中止';
     default:
-      return '資材部承認待ち';
+      return '';
   }
 }
 
@@ -70,7 +70,6 @@ function get_card_header_datas() {
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
   foreach ($results as &$row) {
     if (isset($row['procurement_statuses'])) {
       $statuses = explode(',', $row['procurement_statuses']);
@@ -78,8 +77,8 @@ function get_card_header_datas() {
         $status = map_procurement_status((int)$status);
       }
       $row['procurement_statuses'] = implode(',', $statuses);
-      $row['card_status'] = map_card_status((int)$row['card_status']);
     }
+    $row['card_status'] = map_card_status((int)$row['card_status']);
   }
 
   return $results;
