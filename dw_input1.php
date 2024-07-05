5b6a2b82ea63a15d80bb0970330d25e136ec03bd
taiseikiko <htetwai@taiseikiko.com>
2024-07-05 10:36:06 +0900

    search form of dw_management

---
+++ dw_input1.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+header('Program-id: estimate_entry.php');
+header('Content-type: text/html; charset=utf-8');
+require_once('function.php');
+$_SESSION['token'] = get_csrf_token(); // CSRFのトークンを取得する
+$dept_code = $_SESSION['department_code'];
+include("dw_input1_data_set.php");
+include("header1.php");
+?>
+<main>
+  <h3>【　図面管理　一覧　】</h3>
+
+   <!-- PHP to display result if available -->
+
+  <div class="container">
+    <form id="searchForm" class="row g-3" method="POST" id="dw_input_form">
+      <table style="width:auto;">
+        <tr>
+          <td>
+            <div class="field-row">
+              <label class="common_label" for="class_code">分類</label>
+              <input type="text" id="class_code" name="class_code" value="<?= isset($class_code) ? htmlspecialchars($class_code) : '' ?>">
+
+              <label class="common_label" for="zkm_code">材工名</label>
+              <input type="text" id="zkm_code" name="zkm_code" value="<?= isset($zkm_code) ? htmlspecialchars($zkm_code) : '' ?>">
+
+              <label class="common_label" for="size">サイズ</label>
+              <input type="text" id="size" name="size" value="<?= isset($size) ? htmlspecialchars($size) : '' ?>">
+
+              <label class="common_label" for="joint">接合形状</label>
+              <input type="text" id="joint" name="joint" value="<?= isset($joint) ? htmlspecialchars($joint) : '' ?>">
+
+              <button type="submit" style="margin-left:20px;" id="searchBtn" name="process" value="search" class="search_btn">検索</button>
+            </div>
+          </td>
+        </tr>
+      </table>
+      
+      <table class="tab1" style="margin-top:20px;">
+        <tr>
+          <th>分類</th>
+          <th>材工名</th>
+          <th>サイズ</th>
+          <th>接合形状</th>
+          <th>仕様</th>
+          <th>工事関連図</th>
+          <th>図面</th>
+          <th>最終図面更新日</th>
+          <th>処理</th>
+        </tr>
+        <tr>
+          <td colspan="9">
+            <button type="submit" name="process" value="new">新規登録</button>
+          </td>
+        </tr>
+        <tbody>
+        <?php foreach ($dw_datas as $item): ?>
+        <tr>
+          <td><?= $item['class_code'] ?></td>
+          <td><?= $item['zkm_code'] ?></td>
+          <td><?= $item['size'] ?></td>
+          <td><?= $item['joint'] ?></td>
+          <td><?= $item['specification'] ?></td>
+          <td></td>
+          <td></td>
+          <td><?= $item['upd_date'] ?></td>
+          <td style="text-align:center">
+            <button type="submit" class="updateBtn" data-dw_no="<?= $item['dw_no'] ?>" name="process" value="update">更新</button>
+          </td>
+          <input type="hidden" class="dw_no" name="dw_no" value="">
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+      </table>
+      <?php 
+        if (count($dw_datas) <= 0) {
+          echo "<div><h4 style='font-size: 12px;'>表示するデータがございません。</h4></div>";
+        }
+      ?>
+    </form>
+  </div>
+</main>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="assets/js/customer_ent.js"></script> 
+<script src="assets/js/public_office_ent.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+  
+$(document).ready(function(){
+  // $('#searchBtn').click(function() {
+  //   alert('hereere');
+  //   $('#dw_input_form').attr('action', 'dw_input1_data_set.php');
+  // });
+
+  $(document).on('click', '.updateBtn', function() {
+    var selectedId = $(this).data('sq_no');
+    $('.sq_no').val(selectedId);
+  });
+
+});
+
+/**-------------------------------------------------------------------------------------------------------------- */
+
+function handleWindowClose() {
+  $.ajax({
+    type: 'POST',
+    url: 'sales_request_input1_data_set.php',
+    data: { 
+      return: false,
+      cust_name: cust_name, 
+      pf_name: pf_name },
+    success: function(response) {
+      $('#sq_data_table').html(response);
+    }
+    
+  });
+}
+
+/**-------------------------------------------------------------------------------------------------------------- */
+</script>
+<?php
+include("footer.html");
+?>
---
+++ dw_input1_data_set.php
@@ -0,0 +1,47 @@
+<?php
+require_once('function.php');
+$pdo = new PDO(DNS, USER_NAME, PASSWORD, get_pdo_options());
+//ログインユーザーの部署ID
+$dept_cd = $_POST['dept_code'] ?? $dept_code;
+$dept_id = getDeptId($dept_cd);
+$dw_datas = [];
+
+//検索ボタンを押下した場合
+if (isset($_POST['process']) == 'search') {
+  $class_code = $_POST['class_code']?? '';  //分類
+  $zkm_code = $_POST['zkm_code'] ?? '';     //材工名
+  $size = $_POST['size'] ?? '';             //サイズ
+  $joint = $_POST['joint'] ?? '';           //接合形状
+
+  $sql = "SELECT dw.dw_no, dw.client, 
+          CASE dw.dw_status
+            WHEN 1 THEN '承認待ち'
+            WHEN 2 THEN '完了'
+            WHEN 3 THEN '差し戻し'
+          END AS status, 
+          dw.dw_div1, dw.open_div, dw.class_code, dw.zkm_code, dw.size, dw.joint,
+          dw.pipe, dw.specification, dw.dw_div2, dw.upd_date
+          FROM dw_management_tr dw 
+          WHERE 1 = 1";
+  if (!empty($class_code)) {
+    $search_kw['class_code'] = $class_code;
+    $sql .= " AND dw.class_code LIKE :class_code";
+  }
+  if (!empty($zkm_code)) {
+    $search_kw['zkm_code'] = $zkm_code;
+    $sql .= " AND dw.zkm_code LIKE :zkm_code";
+  }
+  if (!empty($size)) {
+    $search_kw['size'] = $size;
+    $sql .= " AND dw.size LIKE :size";
+  }
+  if (!empty($joint)) {
+    $search_kw['joint'] = $joint;
+    $sql .= " AND dw.joint LIKE :joint";
+  }
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute($search_kw);
+
+  $dw_datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
